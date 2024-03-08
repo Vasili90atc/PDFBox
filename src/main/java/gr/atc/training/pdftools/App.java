43170767b747e4dcf332d88426992265e37ab1f5
Ioanna-Nikoletta Vasili <IVasili@atc.gr>
2024-03-08 12:33:34 +0200

    correct commit

--- src/main/java/gr/atc/training/pdftools/App.java
+++ src/main/java/gr/atc/training/pdftools/App.java
@@ -1,6 +1,5 @@
 package gr.atc.training.pdftools;
 
-
 public class App {
 	public static void main(String[] args) {
 		if (args.length != 2) {
@@ -8,22 +7,29 @@ public class App {
 			System.exit(1);
 		}
 		String pdfPath = args[0];
-		String pageRange = args[1];
+		
+		System.out.println("args[0]: " + args[0]);
+		System.out.println("args[1]: " + args[1]);
+		PDFBox pb = new PDFBox();
+		/*
+		String pageRange = args[2];
 		System.out.println("pdfPath: " + pdfPath);
 		System.out.println("pageRage: " + pageRange);
-
 		int pageStart = -1;
 		int pageEnd = -1;
 		if (pageRange.contains("-")) {
 			String[] nums = pageRange.split("-");
 			pageStart = Integer.parseInt(nums[0]);
 			pageEnd = Integer.parseInt(nums[1]);
+			
 		} else {
 			pageStart = Integer.parseInt(pageRange);
 			pageEnd = Integer.parseInt(pageRange);
 		}
-
-		PDFBox pb = new PDFBox();
 		pb.split(pdfPath, pageStart, pageEnd);
+		*/
+		
+		String pdfPath2 = args[1];
+		pb.merge(pdfPath,pdfPath2);
 	}
 }
--- src/main/java/gr/atc/training/pdftools/PDFBox.java
+++ src/main/java/gr/atc/training/pdftools/PDFBox.java
@@ -2,7 +2,6 @@ package gr.atc.training.pdftools;
 
 import java.io.File;
 import java.io.IOException;
-
 import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.pdmodel.PDDocument;
 
@@ -11,35 +10,63 @@ public class PDFBox {
 	public PDFBox() {
 	}
 
-	public void split(String pdfPath, int pageStart, int pageEnd ) {
-		PDDocument doc = new PDDocument();
-		File file = new File(pdfPath);	
+	public void split(String pdfPath, int pageStart, int pageEnd) {
+		PDDocument doc = null;
+		File file = new File(pdfPath);
 		try {
 			doc = Loader.loadPDF(file);
-		} catch (IOException ex) {
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
-			ex.printStackTrace();
+			e.printStackTrace();
 		}
 		if (pageStart < 1 || pageEnd > doc.getNumberOfPages()) {
 			System.err.println("Invalid page range");
 			System.exit(1);
 		}
-		
 		PDDocument new_doc = new PDDocument();
-		for (int i=pageStart; i<=pageEnd; i++) {
+		for (int i = pageStart; i <= pageEnd; i++) {
 			new_doc.addPage(doc.getPage(i));
 		}
 		try {
-			new_doc.save(pdfPath + "_" + Integer.toString(pageStart) + "-" +Integer.toString(pageEnd) + ".pdf");
+			new_doc.save(pdfPath.split("\\.")[0] + "_" + Integer.toString(pageStart) + "-" + Integer.toString(pageEnd)
+					+ ".pdf");
 			new_doc.close();
 			doc.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
 	}
-	
+
 	public void merge(String pdfPath1, String pdfPath2) {
-		
+		PDDocument doc_enwsi = new PDDocument();
+		PDDocument doc1 = null;
+		PDDocument doc2 = null;
+		File file1 = new File(pdfPath1);
+		File file2 = new File(pdfPath2);
+		try {
+			doc1 = Loader.loadPDF(file1);
+			doc2 = Loader.loadPDF(file2);
+
+		} catch (IOException ex) {
+			// TODO Auto-generated catch block
+			ex.printStackTrace();
+		}
+		for (int j = 0; j < doc1.getNumberOfPages(); j++) {
+			doc_enwsi.addPage(doc1.getPage(j));
+		}
+		for (int j = 0; j < doc2.getNumberOfPages(); j++) {
+			doc_enwsi.addPage(doc2.getPage(j));
+		}
+		try {
+			doc_enwsi.save(pdfPath1.split("\\.")[0] + "_" + pdfPath2.split("\\.")[0] + ".pdf");
+			doc_enwsi.close();
+			doc1.close();
+			doc2.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
